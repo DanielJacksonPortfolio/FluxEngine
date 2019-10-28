095f7186b7a68b0f1aa9cd7df029faa1596d5765
WhoGivesAFlux <lightningstrike0805@gmail.com>
2019-10-28 08:02:35 +0000

    Converted to wWinMain to keep the system based on the original tutorials. May change back if it proves to be annoying.

--- FluxEngine/WinMain.cpp
+++ FluxEngine/WinMain.cpp
@@ -1,5 +1,31 @@
 #include <Windows.h>
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmdLine, int nCmdShow)
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,int nCmdShow)
 {
+	HRESULT hr = CoInitialize(NULL);
+
+	//INIT
+
 	return 0;
-}
+}	
+//
+//int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+//{
+//	HRESULT hr = CoInitialize(NULL);
+//	if (FAILED(hr))
+//	{
+//		ErrorLogger::Log(hr, "Failed to Co-Initialize");
+//		return -1;
+//	}
+//
+//	Engine engine;
+//	if (engine.Init(hInstance, "DirectX Engine", "MyWindowClass", 1600, 900))
+//	{
+//		while (engine.ProcessMessages())
+//		{
+//			engine.Update();
+//			engine.RenderFrame();
+//		}
+//	}
+//	return 0;
+//}
